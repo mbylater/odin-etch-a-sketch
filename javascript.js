e56e7795eb4646e6fafa47b1f67f55cd76fa6a44
MbyLater <185529385+mbylater@users.noreply.github.com>
2025-03-08 18:54:08 -0500

    Set up grid

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
-// Create 16x16 square of divs 
-
 // Select the container
-const container = document.querySelector("#container");
+const container = document.querySelector(".container");
+
+// Create variable to controle number of squares
+let amount = 16 * 16;
+
+// Create a loop
+for (let i=0; i<amount; i++) {
+    // Create 16x16 square of divs and append them to the container
+    const square = document.createElement("div");
+    container.appendChild(square);
+    square.className = "square";
+}
